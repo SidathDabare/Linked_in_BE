ee3dac48a922d84d3b2283af16a69da6c59806c9
SidathDabare <sidath2007@gmail.com>
2023-02-08 23:07:57 +0100

    fix mongo

--- index.js
+++ index.js
@@ -40,6 +40,15 @@ const server = express()
 server.use(cors())
 server.use(express.json())
 
+mongoose.connection.on("connected", () => {
+  console.log("Successfully connected to MongoDB!")
+
+  server.listen(port, () => {
+    console.table(listEndpoints(server))
+    console.log("Server is running on port:", port)
+  })
+})
+
 server.use("/api/posts", postsRouter)
 server.use("/api/users", usersRouter)
 server.use("/api/files", filesRouter)
@@ -50,12 +59,3 @@ server.use(notFoundHandler)
 server.use(genericErrorHandler)
 
 mongoose.connect(process.env.MONGO_CON_URL)
-
-mongoose.connection.on("connected", () => {
-  console.log("Successfully connected to MongoDB!")
-
-  server.listen(port, () => {
-    console.table(listEndpoints(server))
-    console.log("Server is running on port:", port)
-  })
-})
